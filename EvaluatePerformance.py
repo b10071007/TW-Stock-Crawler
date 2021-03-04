3df46e7506c79e1aee2f71ab6a75b0332c2a9706
b10071007 <b10071007@hotmail.com>
2021-03-04 22:12:15 +0800

    Fix the way to evaluate performance

--- EvaluatePerformance.py
+++ EvaluatePerformance.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 def EvaluatePerformance():
     
+    print("[Evaluate Performance]")
     # Set file path
     rootPath = os.getcwd()
     path_stockRecord = os.path.join(rootPath, "data", "UserDefine", "StockRecord.xlsx")
@@ -20,31 +21,89 @@ def EvaluatePerformance():
     
     stockList = pd.unique(stockRecord["Stock"])
     stockReturnSummary = pd.DataFrame(
-            columns=["Stock", "HoldAmount", "BuyAvgPrice", "SellAmount", "SellAvgPrice", "SellReturn"])
+            columns=["股票代號", "股票名稱", "持有股份", "買入均價", "持倉成本", 
+                     "市場價格", "賣出股份", "賣出均價", "賣出金額", 
+                     "持倉總成本", "持倉總價值", "未實現損益", "損益"])
     
     # Read latest price information
-    # latestPrice = pd.read_excel(path_todayPrice, dtype=str)
+    latestPrice = pd.read_excel(path_todayPrice, dtype=str)
 
-    for idx,stock in enumerate(stockList):
+    totalValue = 0
+    totalCost = 0
+    totalProfit = 0
+    for idx, stock in enumerate(stockList):
 
+        # Get stock information
         stockRecord_select = stockRecord[stockRecord["Stock"]==stock]
         buy_Record = stockRecord_select[stockRecord_select["Buy"]=="True"]
         sell_Record = stockRecord_select[stockRecord_select["Buy"]!="True"]
-    
-        buyCost = np.sum(buy_Record["Price"] * buy_Record["Amount"] + 20)
-        sellReturn = np.sum(sell_Record["Price"] * sell_Record["Amount"] * (1-0.003) - 20)
+        
+        # Basic measure
+        totalBuyAmount = np.sum(buy_Record["Amount"])
+        totalBuyValue = np.sum(buy_Record["Price"] * buy_Record["Amount"] + 20)
+        totalSellAmount = np.sum(sell_Record["Amount"])
+        totalSellValue = np.sum(sell_Record["Price"] * sell_Record["Amount"] * (1-0.003) - 20)
         holdAmount = np.sum(buy_Record["Amount"]) - np.sum(sell_Record["Amount"])
-        
-        BuyAvgPrice = (buyCost-sellReturn)/holdAmount if holdAmount!=0 else 0
-        BuyAvgPrice = round(BuyAvgPrice,2)
-        
-        sellAmount = np.sum(sell_Record["Amount"])
-        sellAvgPrice = sellReturn / sellAmount if sellAmount!=0 else 0
 
-        stockReturnSummary.loc[idx] = [stock, holdAmount, BuyAvgPrice, sellAmount, sellAvgPrice, round(sellReturn)]
+        BuyAvgPrice = totalBuyValue / totalBuyAmount
+        BuyAvgPrice = round(BuyAvgPrice, 4)
+
+        SellAvgPrice = totalSellValue / totalSellAmount if totalSellAmount>0 else 0
+        SellAvgPrice = round(SellAvgPrice, 4)
+
+        # Get latest information
+        latestPrice_select = latestPrice[latestPrice["證券代號"]==stock]
+        name_select = str(latestPrice_select.iloc[0,1])
+        price_select = float(latestPrice_select.iloc[0,-2])
+        latestValue = price_select * holdAmount
+
+        # KeepCost and Unrealized profit
+        keepCost = (totalBuyValue - totalSellValue)/ holdAmount if holdAmount>0 else 0
+        keepCostValue = keepCost * holdAmount
+
+        keepProfit = (price_select - keepCost) * holdAmount
+        keepProfitRatio = keepProfit / (latestValue-keepProfit) * 100 if holdAmount>0 else 0
+        keepProfitRatio = round(keepProfitRatio,2)
+        
+        # Realized Profit
+        if (totalSellValue!=0):
+            profit = totalSellValue - (totalBuyValue-keepCostValue)
+        else:
+            profit = 0
+
+        totalProfit += profit
+        totalValue += latestValue
+        totalCost += keepCostValue
+        
+        # ["股票代號", "股票名稱", "持有股份", "買入均價", "持倉成本" 
+        #  "市場價格", "賣出股份", "賣出均價", "賣出總金額", 
+        #  "持倉總成本", "持倉總價值", "未實現損益", "損益"]
+        stockReturnSummary.loc[idx] = [
+                stock, name_select, holdAmount, BuyAvgPrice, keepCost,
+                price_select, totalSellAmount, SellAvgPrice, totalSellValue, 
+                keepCostValue, latestValue, str(keepProfitRatio) + "%", profit]
     
+    
+    # Calculate Total Value
+    UnrealizedProfitRate = (totalValue-totalCost)/totalCost if totalCost!=0 else 0 
+    UnrealizedProfitRate = str(round(UnrealizedProfitRate*100, 2)) + "%"
+
+    totalProfitRate = (totalValue + totalProfit - totalCost) / totalCost
+    totalProfitRate = str(round(totalProfitRate*100, 2)) + "%"
+    
+    stockReturnSummary.loc[idx+1] = [
+            "Total", "-", "-", "-", "-", "-", "-", "-", "-", 
+            totalCost,  totalValue, UnrealizedProfitRate, totalProfit,
+            ]
+
+    stockReturnSummary.loc[idx+2] = [
+            "-", "-", "-", "-", "-", "-", "-", "-", "-",
+            "Total Profit",  totalValue+totalProfit, totalProfitRate, "-",
+            ]
     
     stockReturnSummary.to_excel(path_stockSummary, index=False)
+
+    print("Done")
 
     
 if __name__ == '__main__':
