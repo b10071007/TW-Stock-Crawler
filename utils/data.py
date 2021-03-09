8407baa9a17fb9a17fc37097372aa6c4f346775d
b10071007 <b10071007@hotmail.com>
2021-03-09 23:02:11 +0800

    Modify the data read from dividend record

--- utils/data.py
+++ utils/data.py
@@ -23,7 +23,7 @@ def ReadStockRecord(path_stockRecord):
     stockRecord_raw[["Price", "Amount"]] = stockRecord_raw[["Price", "Amount"]].apply(pd.to_numeric)
     stockRecord_raw["Stock"] = stockRecord_raw["Stock"].astype(str)
     stockRecord_raw["Date"] = stockRecord_raw["Date"].apply(pd.to_datetime)
-    stockRecord_raw["Buy"] = stockRecord_raw["Buy"].astype(bool)
+    stockRecord_raw["Buy"] = stockRecord_raw["Buy"]=="True"
     
     stockList = np.unique(stockRecord_raw['Stock'])
     
@@ -52,6 +52,8 @@ def ReadDividendRecord(path_dividendRecord):
              - columns = ['Year', 'Amount', 'Dividend_unit', 'Dividend_value']
     2. years : Array
         Return the years list
+    3. stockNames : List
+        Return the stock name list
     
     """
     dividendRecord_raw = pd.read_excel(path_dividendRecord, dtype=str, sheet_name = "Record")
@@ -61,15 +63,20 @@ def ReadDividendRecord(path_dividendRecord):
     stockList = [dividendRecord_raw.columns[i] for i in select_index]
     
     dividendRecord = {}
+    stockNames = []
     for idx, stockID in enumerate(stockList):
         stock_position_idx = select_index[idx]
         selectDividend = dividendRecord_raw.iloc[1:, (1+stock_position_idx-1):(1+stock_position_idx-1+3)]
+        stockName = selectDividend.columns[1]
+        
         selectDividend = selectDividend.set_axis(["Amount", "Dividend_unit", "Dividend_value"], axis=1)
         selectDividend.insert(0, "Year", years)
         
         dividendRecord[stockID] = selectDividend
-        
-    return dividendRecord, years
+        stockNames.append(stockName)
+    
+    
+    return dividendRecord, years, stockNames
     
     
     
